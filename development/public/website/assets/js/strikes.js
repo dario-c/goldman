a2b3d8be67f7ecfea47ff1212fdee53961b873b4
dario-c <dariocavanillas@gmail.com>
2015-02-06 15:53:32 +0100

    WIP need to replace lines and animate correctly

--- development/public/website/assets/js/strikes.js
+++ development/public/website/assets/js/strikes.js
@@ -11,51 +11,88 @@
      */
     ns.Strikes = function ()
     {
+        var $window = ns.$win;
         var lines;
         var containers;
         var allStrikes  = [];
+        var scrollThresHold = 200;
 
         var findAllTextLines = function() {
             containers = $(".strike-through");
-            lines = $(".line");
+            lines = containers.find(".line");
         };
 
-        var createStikes = function(linesArray){
+        var createStrikes = function(linesArray){
             for(var x = 0; x < linesArray.length; x++) {
                 var newStrike = {};
                 var $line = $(linesArray[x]);
-                var offset = $line.offset();
+                var lineOffset = $line.offset();
+                var parentOffset = $line.closest(".cross-out").offset();
 
                 newStrike.width = $line.innerWidth();
-                newStrike.height = $line.innerHeight() / 2;  // Be half as big as the span
-                newStrike.top = offset.top + newStrike.height;
-                newStrike.left = offset.left;
+                newStrike.height = $line.innerHeight();
+                newStrike.top = lineOffset.top - parentOffset.top;
+                newStrike.left = lineOffset.left - parentOffset.left;
+                newStrike.offsetPage = parentOffset.top;
+
+                allStrikes.push(newStrike);
+                console.log(parentOffset);
             }
         };
 
         var appendStrikes = function(linesArray, allStrikes){
             for(var x = 0; x < linesArray.length; x++) {
                 var $line = $(linesArray[x]);
-                console.log($line.parent().find("line-container"));
-                var span = $("<span>hello</span>");
-                span.css({ color: "red"});
+
+                var span = $("<span></span>");
+                span.css({ width: allStrikes[x].width, height: allStrikes[x].height, top: allStrikes[x].top, left: allStrikes[x].left});
 
                 $line.closest(".cross-out").find(".line-container").append(span);
             }
         };
 
+        var start = true;
+        var finishScroll;
+
+        var detectScrollLimit = function() {
+            var strikes = allStrikes;
+            for(var x = 0; x < allStrikes.length; x++){
 
 
-        window.onscroll = function() {
+                // If I reach the startpoint 
+                if($window.scrollTop() + scrollThresHold >= allStrikes[x].offsetPage){
+
+                    if(start){
+                        finishScroll = $window.scrollTop();
+                        start = false;
+                    }
+
+
+                    var newWidth = (($window.scrollTop() * allStrikes[0].width) / finishScroll) - (allStrikes[0].width);
+
+                    console.log(newWidth);
+
+                    // if(newWidth < allStrikes[x].width) {
+                        var span = $(".line-container").find("span")[x];
+                        $(span).css({ width: newWidth });
+                    // }
+
+                }
+            }
         };
 
         var init = function ()
         {
            console.log(document.body.scrollTop);
            findAllTextLines();
-           createStikes(lines);
+           createStrikes(lines);
            appendStrikes(lines, allStrikes);
-           // console.log(allStrikes);
+           
+            // $window.on("scroll", function()
+            // {
+            //     window.requestAnimationFrame(detectScrollLimit);
+            // });
+
         };
 
         init();
